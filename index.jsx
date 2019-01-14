049947738b1fa5cda017024a408d196694b7ba87
moriyuu <moriyu54321@gmail.com>
2019-01-15 06:18:48 +0900

    update

--- index.jsx
+++ index.jsx
@@ -4,16 +4,120 @@ import "reference-tooltip";
 
 const App = () => {
   return (
-    <div>
+    <div style={{ width: "50%", minWidth: "350px", margin: "auto" }}>
       <h1>reference-tooltip-react-sample</h1>
-      <p style={{ textAlign: "left" }}>
-        left foo <referenced-word rwid="la">Los Angeles</referenced-word> bar
+      <p>
+        <a href="https://github.com/moriyuu/reference-tooltip-react-sample">
+          https://github.com/moriyuu/reference-tooltip-react-sample
+        </a>
       </p>
-      <p style={{ textAlign: "right" }}>
-        right foo <referenced-word rwid="la">Los Angeles</referenced-word> bar
+
+      <p>
+        La La Land is a 2016 American romantic musical film written and directed
+        by Damien Chazelle. It stars
+        <referenced-word rwid="ryan_gosling">Ryan Gosling</referenced-word> as a
+        jazz pianist and
+        <referenced-word rwid="emma_stone">Emma Stone</referenced-word> as an
+        aspiring actress, who meet and fall in love in
+        <referenced-word rwid="los_angeles">Los Angeles</referenced-word> while
+        pursuing their dreams.
       </p>
-      <p style={{ position: "absolute", bottom: "40px" }}>
-        bottom foo <referenced-word rwid="la">Los Angeles</referenced-word> bar
+      <p>
+        Having been fond of musicals during his time as a drummer, Chazelle
+        first conceptualised the film alongside Justin Hurwitz while attending
+        Harvard University together. Moving to
+        <referenced-word rwid="los_angeles">Los Angeles</referenced-word> in
+        2010, Chazelle wrote the screenplay but did not find a studio willing to
+        finance the production without changes to his design. Following the
+        success of his 2014 film Whiplash, the project was picked up by Summit
+        Entertainment. Filming took place in
+        <referenced-word rwid="los_angeles">Los Angeles</referenced-word> from
+        August to September 2015, with the film's score composed by Hurwitz and
+        the dance choreography by Mandy Moore. La La Land premiered at the 73rd
+        Venice International Film Festival on August 31, 2016, and was released
+        in the United States on December 9, 2016. It grossed $446 million
+        worldwide against a production budget of $30 million.
+      </p>
+
+      <p>
+        La La Land is a 2016 American romantic musical film written and directed
+        by Damien Chazelle. It stars
+        <referenced-word rwid="ryan_gosling">Ryan Gosling</referenced-word> as a
+        jazz pianist and
+        <referenced-word rwid="emma_stone">Emma Stone</referenced-word> as an
+        aspiring actress, who meet and fall in love in
+        <referenced-word rwid="los_angeles">Los Angeles</referenced-word> while
+        pursuing their dreams.
+      </p>
+      <p>
+        Having been fond of musicals during his time as a drummer, Chazelle
+        first conceptualised the film alongside Justin Hurwitz while attending
+        Harvard University together. Moving to
+        <referenced-word rwid="los_angeles">Los Angeles</referenced-word> in
+        2010, Chazelle wrote the screenplay but did not find a studio willing to
+        finance the production without changes to his design. Following the
+        success of his 2014 film Whiplash, the project was picked up by Summit
+        Entertainment. Filming took place in
+        <referenced-word rwid="los_angeles">Los Angeles</referenced-word> from
+        August to September 2015, with the film's score composed by Hurwitz and
+        the dance choreography by Mandy Moore. La La Land premiered at the 73rd
+        Venice International Film Festival on August 31, 2016, and was released
+        in the United States on December 9, 2016. It grossed $446 million
+        worldwide against a production budget of $30 million.
+      </p>
+
+      <p>
+        La La Land is a 2016 American romantic musical film written and directed
+        by Damien Chazelle. It stars
+        <referenced-word rwid="ryan_gosling">Ryan Gosling</referenced-word> as a
+        jazz pianist and
+        <referenced-word rwid="emma_stone">Emma Stone</referenced-word> as an
+        aspiring actress, who meet and fall in love in
+        <referenced-word rwid="los_angeles">Los Angeles</referenced-word> while
+        pursuing their dreams.
+      </p>
+      <p>
+        Having been fond of musicals during his time as a drummer, Chazelle
+        first conceptualised the film alongside Justin Hurwitz while attending
+        Harvard University together. Moving to
+        <referenced-word rwid="los_angeles">Los Angeles</referenced-word> in
+        2010, Chazelle wrote the screenplay but did not find a studio willing to
+        finance the production without changes to his design. Following the
+        success of his 2014 film Whiplash, the project was picked up by Summit
+        Entertainment. Filming took place in
+        <referenced-word rwid="los_angeles">Los Angeles</referenced-word> from
+        August to September 2015, with the film's score composed by Hurwitz and
+        the dance choreography by Mandy Moore. La La Land premiered at the 73rd
+        Venice International Film Festival on August 31, 2016, and was released
+        in the United States on December 9, 2016. It grossed $446 million
+        worldwide against a production budget of $30 million.
+      </p>
+
+      <p>
+        La La Land is a 2016 American romantic musical film written and directed
+        by Damien Chazelle. It stars
+        <referenced-word rwid="ryan_gosling">Ryan Gosling</referenced-word> as a
+        jazz pianist and
+        <referenced-word rwid="emma_stone">Emma Stone</referenced-word> as an
+        aspiring actress, who meet and fall in love in
+        <referenced-word rwid="los_angeles">Los Angeles</referenced-word> while
+        pursuing their dreams.
+      </p>
+      <p>
+        Having been fond of musicals during his time as a drummer, Chazelle
+        first conceptualised the film alongside Justin Hurwitz while attending
+        Harvard University together. Moving to
+        <referenced-word rwid="los_angeles">Los Angeles</referenced-word> in
+        2010, Chazelle wrote the screenplay but did not find a studio willing to
+        finance the production without changes to his design. Following the
+        success of his 2014 film Whiplash, the project was picked up by Summit
+        Entertainment. Filming took place in
+        <referenced-word rwid="los_angeles">Los Angeles</referenced-word> from
+        August to September 2015, with the film's score composed by Hurwitz and
+        the dance choreography by Mandy Moore. La La Land premiered at the 73rd
+        Venice International Film Festival on August 31, 2016, and was released
+        in the United States on December 9, 2016. It grossed $446 million
+        worldwide against a production budget of $30 million.
       </p>
     </div>
   );
